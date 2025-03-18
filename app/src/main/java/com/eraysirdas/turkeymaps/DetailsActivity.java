422a24101835921a010e2447850f715aaa588015
eraysirdas <eraysrds@gmail.com>
2025-03-18 14:02:33 +0300

    first commit

---
+++ app/src/main/java/com/eraysirdas/turkeymaps/DetailsActivity.java
@@ -0,0 +1,39 @@
+package com.eraysirdas.turkeymaps;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Objects;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        String id = intent.getStringExtra("id");
+
+        Objects.requireNonNull(getSupportActionBar()).setTitle(name+" "+ id);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
